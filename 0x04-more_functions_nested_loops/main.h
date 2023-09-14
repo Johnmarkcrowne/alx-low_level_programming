2920c96107742897cba86ebb847a63b91ac535c0
Johnmarkcrowne <johnmarkmurithi04@gmail.com>
2023-09-14 08:47:58 -0700

    Task zero

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int_putchar(char C);
+int_isupper(int C);
+int_isdigit(int C);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangel(int size);
+
+#endif
