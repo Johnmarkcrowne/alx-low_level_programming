4ea5c26a83f5f486d8c10022cd56388b8095a740
Johnmarkcrowne <johnmarkmurithi04@gmail.com>
2023-09-20 03:59:53 -0700

    Task twelve

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -36,7 +36,7 @@ if (n >= 0 && n <= 15)
 			_putchar(' ');
 			_putchar((k / 100) + '0');
 			_putchar(((k / 10) % 10) + '0');
-			_putchar((k % 10) + '0')
+			_putchar((k % 10) + '0');
 		}
 		}
 		_putchar('\n');
